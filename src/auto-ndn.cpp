7cff2eb8a927778240ab216e8aec66eb8557cf85
Mini-NDN <Mini-NDN@europa.cs.memphis.edu>
2017-02-02 12:07:50 -0500

    Fixed "TLV length exceeds buffer length" error

--- src/auto-ndn.cpp
+++ src/auto-ndn.cpp
@@ -16,6 +16,7 @@ AutoNdn::AutoNdn(ndn::Face& face, ndn::util::Scheduler& scheduler)
 
   void
   AutoNdn::run() {
+    std::cout << "In AutoNdn::run() " << std::endl;
     initialize();
     m_control.run();
 
@@ -30,7 +31,8 @@ AutoNdn::AutoNdn(ndn::Face& face, ndn::util::Scheduler& scheduler)
   void
   AutoNdn::initializeKey() {
     ndn::Name defaultIdentity = m_confParameter.getCarName();
-
+    //ndn::Name defaultIdentity = "/test";
+    std::cout << "Default Indentity: " << defaultIdentity << std::endl;
     try {
       m_keyChain.deleteIdentity(defaultIdentity);
     }
@@ -39,14 +41,14 @@ AutoNdn::AutoNdn(ndn::Face& face, ndn::util::Scheduler& scheduler)
 
     m_signingInfo = ndn::security::SigningInfo(ndn::security::SigningInfo::SIGNER_TYPE_ID, defaultIdentity);
     ndn::Name keyName = m_keyChain.generateRsaKeyPairAsDefault(defaultIdentity, true);
-    /*keyname*/ std::cout << keyName.toUri() << std::endl;
+    /*keyname*/ std::cout << "Keyname: " << keyName.toUri() << std::endl;
     ndn::shared_ptr<ndn::IdentityCertificate> certificate =
                                               ndn::make_shared<ndn::IdentityCertificate>();
     ndn::shared_ptr<ndn::PublicKey> pubKey = m_keyChain.getPublicKey(keyName);
 
     ndn::Name certificateName = keyName.getPrefix(-1);
     certificateName.append("KEY").append(keyName.get(-1)).append("ID-CERT").appendVersion();
-
+    std::cout << "Certificate name: " << certificateName.toUri() << std::endl;
     certificate->setName(certificateName);
     certificate->setNotBefore(ndn::time::system_clock::now() - ndn::time::days(1));
     certificate->setNotAfter(ndn::time::system_clock::now() + ndn::time::days(7300)); // ~20 years
@@ -54,7 +56,7 @@ AutoNdn::AutoNdn(ndn::Face& face, ndn::util::Scheduler& scheduler)
     certificate->addSubjectDescription(ndn::CertificateSubjectDescription(ndn::oid::ATTRIBUTE_NAME,
                                        keyName.toUri()));
     certificate->encode();
-    //m_keyChain.signByIdentity(*certificate, m_confParam.getManufacturerPrefix());
+    m_keyChain.signByIdentity(*certificate, m_confParameter.getCarMake());
 
     m_keyChain.addCertificateAsIdentityDefault(*certificate);
     loadCertToPublish(certificate);
@@ -65,6 +67,7 @@ AutoNdn::AutoNdn(ndn::Face& face, ndn::util::Scheduler& scheduler)
   void
   AutoNdn::initialize() {
     m_confParameter.buildCarName();
-    //initializeKey();
+    std::cout << m_confParameter.getCarName() << std::endl; 
+    initializeKey();
   }
 } // end of namespace autondn
