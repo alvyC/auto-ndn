e80bba492fe5cc81431329737e805a9352d16303
Alvy <muktadir.rahman.c@gmail.com>
2017-02-03 18:28:58 -0500

    comment out initialize key

--- src/auto-ndn.cpp
+++ src/auto-ndn.cpp
@@ -30,39 +30,6 @@ AutoNdn::AutoNdn(ndn::Face& face, ndn::util::Scheduler& scheduler)
 
   void
   AutoNdn::initializeKey() {
-    /*ndn::Name defaultIdentity = m_confParameter.getCarName();
-    //ndn::Name defaultIdentity = "/test";
-    std::cout << "Default Identity: " << defaultIdentity << std::endl;
-    try {
-      m_keyChain.deleteIdentity(defaultIdentity);
-    }
-      catch (std::exception& e) {
-    }
-
-    m_signingInfo = ndn::security::SigningInfo(ndn::security::SigningInfo::SIGNER_TYPE_ID, defaultIdentity);
-    ndn::Name keyName = m_keyChain.generateRsaKeyPairAsDefault(defaultIdentity, true);
-    /*keyname*/ std::cout << "Keyname: " << keyName.toUri() << std::endl;
-    /*ndn::shared_ptr<ndn::IdentityCertificate> certificate =
-                                              ndn::make_shared<ndn::IdentityCertificate>();
-    ndn::shared_ptr<ndn::PublicKey> pubKey = m_keyChain.getPublicKey(keyName);
-
-    ndn::Name certificateName = keyName.getPrefix(-1);
-    certificateName.append("KEY").append(keyName.get(-1)).append("ID-CERT").appendVersion();
-    std::cout << "Certificate name: " << certificateName.toUri() << std::endl;
-    certificate->setName(certificateName);
-    certificate->setNotBefore(ndn::time::system_clock::now() - ndn::time::days(1));
-    certificate->setNotAfter(ndn::time::system_clock::now() + ndn::time::days(7300)); // ~20 years
-    certificate->setPublicKeyInfo(*pubKey);
-    certificate->addSubjectDescription(ndn::CertificateSubjectDescription(ndn::oid::ATTRIBUTE_NAME,
-                                       keyName.toUri()));
-    certificate->encode();
-    m_keyChain.signByIdentity(*certificate, m_confParameter.getCarMake());
-
-    m_keyChain.addCertificateAsIdentityDefault(*certificate);
-    loadCertToPublish(certificate);
-
-    m_defaultCertName = certificate->getName();*/
-
     m_keyChain.addIdentity(m_confParameter.getCarName());
   }
 
